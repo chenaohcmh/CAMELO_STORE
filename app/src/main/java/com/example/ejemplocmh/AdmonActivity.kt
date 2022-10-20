92c12aade2458347e1ed1fc7d2724ed29e7c8557
chenaohcmh <chenaoh@gmail.com>
2022-10-20 18:56:44 -0500

    Primera version proyecto

---
+++ app/src/main/java/com/example/ejemplocmh/AdmonActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ejemplocmh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdmonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admon)
+        setSupportActionBar(findViewById(R.id.My_toolbar))
+    }
+}
